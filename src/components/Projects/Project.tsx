0a3cb64775a8c593c3cf70f157277484dbf0b730
rl16432 <rluo52237@gmail.com>
2023-07-25 20:38:57 +1200

    feat: enlarge source code button

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -38,7 +38,7 @@ const Project = (props: IProjectProps) => (
       <a
         href={props.source}
         target="_blank"
-        className="mt-5 w-10 flex-none hover:translate-y-1 md:ml-auto md:mr-5 md:mt-0"
+        className="mt-5 w-12 flex-none hover:translate-y-1 md:ml-auto md:mr-5 md:mt-0"
       >
         <img className="w-full" src="/assets/images/source-icon.svg" />
       </a>
