4ac664fe80cec363359805f0e73a827ffd1d8d16
rl16432 <rluo52237@gmail.com>
2023-07-28 10:53:07 +1200

    feat: update site description and author

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = {
   site_name: 'Raymond Luo | Personal Site',
   title: 'Raymond Luo',
-  description: 'My personal site',
-  author: 'Raymond',
+  description:
+    "Hi there, I'm Raymond. Penultimate Year Software Engineering Student @ UoA. I love to combine my skills and interests to create memorable experiences for the people around me.",
+  author: 'Raymond Luo',
   locale_region: 'en-us',
   locale: 'en',
 };
