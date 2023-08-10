124b4c15b8eeffb5764b926789a6253a86082e89
rl16432 <rluo52237@gmail.com>
2023-08-10 23:46:48 +1200

    style: run eslint fix

--- src/components/FooterCopyright.tsx
+++ src/components/FooterCopyright.tsx
@@ -5,8 +5,8 @@ type IFooterCopyrightProps = {
 const FooterCopyright = (props: IFooterCopyrightProps) => (
   <div className="border-t border-gray-600 pt-5">
     <div className="text-sm text-gray-200">
-      © Copyright {new Date().getFullYear()} by {props.site_name}. Built with ♥
-      by{' '}
+      © Copyright {new Date().getFullYear()} by {props.site_name}. Built with
+      ♥ by{' '}
       <a
         className="text-cyan-400 hover:underline"
         href="https://creativedesignsguru.com"
