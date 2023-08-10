94af293b014d65bc76242b0373ca3a8bfa8d1bde
rl16432 <rluo52237@gmail.com>
2023-08-10 23:46:23 +1200

    build: update dependencies and resolve breaking changes

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pluginSearchDirs: [__dirname],
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
