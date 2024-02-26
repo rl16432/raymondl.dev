9f18764e27cfa1e99927aa2f3e8d3a5a8ec19491
rl16432 <rluo52237@gmail.com>
2024-02-26 17:10:04 +1300

    feat: custom name for pagination component

--- src/components/NewerOlderPagination.tsx
+++ src/components/NewerOlderPagination.tsx
@@ -2,13 +2,18 @@ import type { Page } from 'astro';
 import type { CollectionEntry } from 'astro:content';
 
 type INewerOlderPaginationProps = {
+  name: String;
   page: Page<CollectionEntry<any>>;
 };
 
 const NewerOlderPagination = (props: INewerOlderPaginationProps) => (
   <div className="flex justify-center gap-8">
-    {props.page.url.prev && <a href={props.page.url.prev}>← Newer Posts</a>}
-    {props.page.url.next && <a href={props.page.url.next}>Older Posts →</a>}
+    {props.page.url.prev && (
+      <a href={props.page.url.prev}>← Newer {props.name}</a>
+    )}
+    {props.page.url.next && (
+      <a href={props.page.url.next}>Older {props.name} →</a>
+    )}
   </div>
 );
 
